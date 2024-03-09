48fb8782f88c32854ed726769edb360ef9d743cf
Lyprandos Liatsos <102907631+Liprandipus@users.noreply.github.com>
2024-03-09 18:39:55 +0200

    Update reverseString.c

--- reverseString.c
+++ reverseString.c
@@ -1,15 +1,16 @@
 void reverseString(char* s, int sSize) {
-    int start =0;
-    int end = sSize-1;
-    int temp;
-    while (start<end)
+    int start =0; //set a pointer to the beginning of the string
+    int end = sSize-1; //set another pointer to the end of the string
+    int temp; // temp value 
+    while (start<end) // 
 {   
-   temp = s[start];
+    // you swap each value 
+   temp = s[start]; 
    s[start] = s[end];
    s[end] = temp;
 
     start++;
     end--;
-
+     // you get the string reversed ( notice that in odd-numbered strings, the middle elemnt isn't important. It remains always in the same place.
 
 }}
