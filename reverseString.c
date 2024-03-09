a95d82707ae60ed4c2a42b1a31b524185f9095d9
Lyprandos Liatsos <102907631+Liprandipus@users.noreply.github.com>
2024-03-09 18:34:11 +0200

    Create reverseString.c

---
+++ reverseString.c
@@ -0,0 +1,15 @@
+void reverseString(char* s, int sSize) {
+    int start =0;
+    int end = sSize-1;
+    int temp;
+    while (start<end)
+{   
+   temp = s[start];
+   s[start] = s[end];
+   s[end] = temp;
+
+    start++;
+    end--;
+
+
+}}
