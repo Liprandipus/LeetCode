a60e570b9d8cd1ec364c9c20b1c03243d120d366
Lyprandos Liatsos <102907631+Liprandipus@users.noreply.github.com>
2024-03-09 18:37:27 +0200

    Update getCommon.c

--- getCommon.c
+++ getCommon.c
@@ -1,14 +1,14 @@
 int getCommon(int* nums1, int nums1Size, int* nums2, int nums2Size) {
            int i = 0 , j = 0 ;
            while ( i < nums1Size && j < nums2Size){
-               if (nums1[i] == nums2[j]){
+               if (nums1[i] == nums2[j]){ // if there are equal, you return the value
                    return nums1[i];
                }
-              else if (nums1[i] < nums2[j]){      
+              else if (nums1[i] < nums2[j]){       // if the first is smaller than the second, you move to the next element
                    i++;   
 
                }
-               else{
+               else{ //else if the second number is smaller than the first, you move to the next element
                    j++;
 
                }
